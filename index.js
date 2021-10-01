3294aab630217fd349082cbe4ecd10679d7d952c
Alexandre Bekor <contato@alexandrebekor.com>
2021-10-01 00:56:20 -0300

    Added ejs

--- index.js
+++ index.js
@@ -2,9 +2,26 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-// Routes
+// Database
+const mongoose = require('mongoose')
+const database = "mongodb://localhost/webapp-parkinglot"
+mongoose.Promise = global.Promise
+
+// Template
+const path = require('path')
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+// Routing
 const PageRoutes = require('./routes/page')
 app.get('/', PageRoutes)
 
-app.listen(port, () => { console.log('Running server ' + port) })
+mongoose
+    .connect(database)
+    .then(() => {
+        app.listen(port, () => { console.log('Running server ' + port) })
+    })
+    .catch(e => console.log(e))
