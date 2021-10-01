4b545d43dde5204ecae086a8c30aec95855904ab
Alexandre Bekor <contato@alexandrebekor.com>
2021-10-01 00:43:25 -0300

    Initialize Project

---
+++ sort.js
@@ -0,0 +1,45 @@
+function reserved(items, item) {
+    const result = items[item]
+    items.splice(item, 1)
+    return result
+}
+
+function randomItem(items) {
+    const randomIndex = Math.floor(
+        Math.random() * items.length
+    )
+    
+    if(items.length > 0) {
+        const result = reserved(items, randomIndex)
+        return result
+    }
+}
+
+const vagas = ["15", "30", "20", "25", "32"]
+const ap = ["44", "12", "22", "32"]
+
+const sorteio = ap.map(num => {
+    return {
+        apartamento: num,
+        vaga: randomItem(vagas)
+    }
+})
+
+const apartamento = {
+    id: 44,
+    status: 0,
+    flag: false,
+    parking: parking
+}
+
+const parking = {
+    name: "25-s",
+    type: "1"
+}
+
+const parking = {
+    name: "27-s",
+    type: "2"
+}
+
+console.log(sorteio)
